a9b018468d351fb80cdc8c36fb68b30f521b74b8
Roberto Cornacchia <roberto@spinque.com>
2020-12-31 00:53:12 +0100

    Complete check_move()

--- src/main.rs
+++ src/main.rs
@@ -149,28 +149,114 @@ impl Board {
 		 Allows to "see through" a specified number of obstacles
 	*/
 	fn check_move(&self, f_pos: &Pos, t_pos: &Pos, max_obstacles: u8) -> MoveType {
+
+		/* Checks possible moves from a given point in all directions */
+		fn check_arm(b:&Board, f_pos:&Pos, t_pos:&Pos, max_obstacles: u8, 
+					f_c:fn(i8,i8)->i8, f_r:fn(i8,i8)->i8) -> MoveType {
+			let mut obstacles = 0;
+			for i in 1..8 {
+				if let Some(x_pos) = Pos::at(f_c(f_pos.col,i), f_r(f_pos.row,i)) {
+					let move_type = b.move_type(f_pos,&x_pos);
+					match &move_type {
+						MoveType::Illegal | MoveType::Eat => {
+							if obstacles == max_obstacles {
+								if *t_pos == x_pos {
+									return move_type;
+								} else {
+									return MoveType::Illegal;
+								}
+							};
+							obstacles = obstacles + 1;
+						},
+						MoveType::Move => {
+							if *t_pos == x_pos {
+								return move_type;
+							}
+						}
+					}
+				} else { 
+					return MoveType::Illegal; 
+				}
+			}
+			return MoveType::Illegal;
+		};
+
+		fn check_bishop(b:&Board, f_pos:&Pos, t_pos:&Pos, max_obstacles:u8) -> MoveType {
+			let add = ops::Add::add;
+			let sub = ops::Sub::sub;
+			
+			if (t_pos.col - f_pos.col).abs() != (t_pos.row - f_pos.row).abs () {
+				return MoveType::Illegal;
+			}
+			if t_pos.col < f_pos.col && t_pos.row > f_pos.row {
+				/* north-west */
+				return check_arm(b,f_pos,t_pos,max_obstacles,sub,add);
+			}
+			if t_pos.col > f_pos.col && t_pos.row > f_pos.row {
+				/* north-east */
+				return check_arm(b,f_pos,t_pos,max_obstacles,add,add);
+			}
+			if t_pos.col > f_pos.col && t_pos.row < f_pos.row {
+				/* south-east */
+				return check_arm(b,f_pos,t_pos,max_obstacles,add,sub);
+			}
+			if t_pos.col < f_pos.col && t_pos.row < f_pos.row {
+				/* south-west */
+				return check_arm(b,f_pos,t_pos,max_obstacles,sub,sub);
+			}
+			return MoveType::Illegal;
+		}
+
+		fn check_rook(b:&Board, f_pos:&Pos, t_pos:&Pos, max_obstacles:u8) -> MoveType {
+			let add = ops::Add::add;
+			let sub = ops::Sub::sub;
+			let id = |x,_| x;
+			
+			if t_pos.col == f_pos.col && t_pos.row > f_pos.row {
+				/* north */
+				return check_arm(b,f_pos,t_pos,max_obstacles,id,add);
+			}
+			if t_pos.col > f_pos.col && t_pos.row == f_pos.row {
+				/* east */
+				return check_arm(b,f_pos,t_pos,max_obstacles,add,id);
+			}
+			if t_pos.col == f_pos.col && t_pos.row < f_pos.row {
+				/* south */
+				return check_arm(b,f_pos,t_pos,max_obstacles,id,sub);
+			}
+			if t_pos.col < f_pos.col && t_pos.row == f_pos.row {
+				/* west */
+				return check_arm(b,f_pos,t_pos,max_obstacles,sub,id);
+			}
+			return MoveType::Illegal;
+		}
+
 		if let Some(f_tile) = self.at(f_pos) {
 			match f_tile.piece {
 				Piece::Pawn => {
-					match f_tile.color {
-						Color::White => {
-							if (t_pos.row == f_pos.row+1 || (f_pos.row == 1 && t_pos.row == 3)) && t_pos.col == f_pos.col && self.at(t_pos).is_none() {
+					let f_incr:fn(i8,i8)->i8;
+					let start_row:i8;
+
+					if f_tile.color == Color::White {
+						f_incr = ops::Add::add;
+						start_row = 1;
+					} else {
+						f_incr = ops::Sub::sub;
+						start_row = 6;
+					}
+					if let Some(t_tile) = self.at(t_pos) {
+						/* Eat diagonally */
+						if (t_pos.col -f_pos.col).abs() == 1 &&
+								t_pos.row == f_incr(f_pos.row,1) &&
+								t_tile.color != f_tile.color {
+							return MoveType::Eat;
+						}
+					} else {
+						/* forward by 1 or 2 */
+						if t_pos.col == f_pos.col {
+							if t_pos.row == f_incr(f_pos.row,1) ||
+									(f_pos.row == start_row && t_pos.row == f_incr(start_row,2)) {
 								return MoveType::Move;
-							}
-							if let Some(t_tile) = self.at(t_pos) {
-								if t_pos.row == f_pos.row+1 && (t_pos.col == f_pos.col-1 || t_pos.col == f_pos.col+1) && t_tile.color != f_tile.color {
-									return MoveType::Eat;
-								}
-							}
-						},
-						Color::Black => {
-							if (t_pos.row == f_pos.row-1 || (f_pos.row == 6 && t_pos.row == 4)) && t_pos.col == f_pos.col && self.at(t_pos).is_none() {
-								return MoveType::Move;
-							}
-							if let Some(t_tile) = self.at(t_pos) {
-								if t_pos.row == f_pos.row-1 && (t_pos.col == f_pos.col-1 || t_pos.col == f_pos.col+1) && t_tile.color != f_tile.color {
-									return MoveType::Eat;
-								}
 							}
 						}
 					}
@@ -190,54 +276,27 @@ impl Board {
 				},
 
 				Piece::Bishop => {
-					if (f_pos.row-t_pos.row).abs() == (f_pos.col-t_pos.col).abs() {
-						if let Some(t_tile)	= self.at(t_pos) {
-							if t_tile.color != f_tile.color {
-								return MoveType::Eat;
-							}
-						} else {
-							return MoveType::Move;
-						}
-					}
+					return check_bishop(self,f_pos,t_pos,max_obstacles);
 				},
 
 				Piece::Rook => {
-					if f_pos.row == t_pos.row || f_pos.col ==t_pos.col {
-						if let Some(t_tile)	= self.at(t_pos) {
-							if t_tile.color != f_tile.color {
-								return MoveType::Eat;
-							}
-						} else {
-							return MoveType::Move;
-						}
-					}
+					return check_rook(self,f_pos,t_pos,max_obstacles);
 				},
 
 				Piece::Queen => {
-					if (f_pos.row-t_pos.row).abs() == (f_pos.col-t_pos.col).abs() || 
-							f_pos.row == t_pos.row || f_pos.col ==t_pos.col {
-						if let Some(t_tile)	= self.at(t_pos) {
-							if t_tile.color != f_tile.color {
-								return MoveType::Eat;
-							}
-						} else {
-							return MoveType::Move;
-						}
+					let cb = check_bishop(self,f_pos,t_pos,max_obstacles);
+					if cb != MoveType::Illegal {
+						return cb;
 					}
+					return check_rook(self,f_pos,t_pos,max_obstacles);
 				},
 
 				Piece::King => {
-					if (f_pos.row-t_pos.row).abs() <= 1 && 
-							(f_pos.col-t_pos.col).abs() <= 1 &&
-							*f_pos != *t_pos {
-						if let Some(t_tile)	= self.at(t_pos) {
-							if t_tile.color != f_tile.color {
-								return MoveType::Eat;
-							}
-						} else {
-							return MoveType::Move;
-						}
+					let cb = check_bishop(self,f_pos,t_pos,max_obstacles);
+					if cb != MoveType::Illegal {
+						return cb;
 					}
+					return check_rook(self,f_pos,t_pos,max_obstacles);
 				},
 			}
 		}
