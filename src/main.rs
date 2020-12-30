15a8c3abcd191c937f556c399254fb8198cadceb
Roberto Cornacchia <roberto@spinque.com>
2020-12-30 17:08:16 +0100

    Refactor and complete move generation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 // use std::io::{self, BufRead};
 use std::cmp;
+use std::ops;
 // use std::fmt::Display;
 // use trees::{tr,Tree};
 
@@ -20,8 +21,51 @@ enum Piece {
 	King,
 }
 
+#[derive(Debug,Copy,Clone,PartialEq,Eq)]
+struct Pos {
+	// we use i8 instead of u8
+	// to avoid overflows when generating positions that are possibly outside the board
+	row: i8,
+	col: i8,
+}
+
+impl Pos {
+	fn at(col: i8, row:i8) -> Option<Pos> {
+		if col < 0 || row < 0 || col > 7 || row > 7 {
+			return None;
+		}
+		return Some(Pos {
+			col: col,
+			row: row,
+	});
+	}
+
+	fn from_coord(x: char, y: char) -> Option<Pos> {
+		if ! (x.is_ascii() && y.is_ascii()) {
+			return None;
+		}
+		let x = x.to_ascii_uppercase();
+
+		if ! (x.ge(&'A') && x.le(&'H') && y.ge(&'1') && y.le(&'8')) {
+			return None;
+		}
+
+		return Some(Pos {
+			col: ((x as u8) - b'A') as i8,
+			row: ((y as u8) - b'1') as i8,
+		});
+	}
+
+	fn to_coord(&self) -> (char,char) {
+		return (
+			((self.col as u8) + b'A') as char,
+			((self.row as u8) + b'1') as char
+		);
+	}
+}
+
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
-enum MoveType { Invalid, Eat, Move }
+enum MoveType { Illegal, Eat, Move }
 
 impl Valuable for Piece {
 	fn value(&self, _for_color: Color) -> u32 {
@@ -85,16 +129,15 @@ impl Board {
 		b
 	}
 
-	fn tile_at_pos(&self, pos: &Pos) -> Option<Tile> {
-		self.tiles[pos.row as usize][pos.col as usize]
-	}
-	fn tile_at(&self, col: i8, row: i8) -> Option<Tile> {
-		self.tiles[row as usize][col as usize]
+	fn tile_at_pos(&self, pos: &Pos) -> &Option<Tile> {
+		&self.tiles[pos.row as usize][pos.col as usize]
 	}
 
-	fn move_type(&self, f_pos: &Pos, t_pos: &Pos) -> MoveType {
-		// TODO: no move is valid if it makes its own king under check
-		// But it needs to be checked on the board with the move already applied
+	/* Checks whether a move is valid
+		 Not needed for generated moves, only for human moves
+		 Allows to "see through" a specified number of obstacles
+	*/
+	fn check_move(&self, f_pos: &Pos, t_pos: &Pos, max_obstacles: u8) -> MoveType {
 		if let Some(f_tile) = &self.tile_at_pos(f_pos) {
 			match f_tile.piece {
 				Piece::Pawn => {
@@ -135,57 +178,164 @@ impl Board {
 					}
 				},
 
-				Piece::Bishop | Piece::Queen => {
-
+				Piece::Bishop => {
+					if (f_pos.row-t_pos.row).abs() == (f_pos.col-t_pos.col).abs() {
+						if let Some(t_tile)	= &self.tile_at_pos(t_pos) {
+							if t_tile.color != f_tile.color {
+								return MoveType::Eat;
+							}
+						} else {
+							return MoveType::Move;
+						}
+					}
 				},
 
-				Piece::Rook | Piece::Queen => {
-
+				Piece::Rook => {
+					if f_pos.row == t_pos.row || f_pos.col ==t_pos.col {
+						if let Some(t_tile)	= &self.tile_at_pos(t_pos) {
+							if t_tile.color != f_tile.color {
+								return MoveType::Eat;
+							}
+						} else {
+							return MoveType::Move;
+						}
+					}
 				},
 
-				Piece::Rook | Piece::Queen => {
-
+				Piece::Queen => {
+					if (f_pos.row-t_pos.row).abs() == (f_pos.col-t_pos.col).abs() || 
+							f_pos.row == t_pos.row || f_pos.col ==t_pos.col {
+						if let Some(t_tile)	= &self.tile_at_pos(t_pos) {
+							if t_tile.color != f_tile.color {
+								return MoveType::Eat;
+							}
+						} else {
+							return MoveType::Move;
+						}
+					}
 				},
-				_ => ()
 
+				Piece::King => {
+					if (f_pos.row-t_pos.row).abs() <= 1 && 
+							(f_pos.col-t_pos.col).abs() <= 1 &&
+							*f_pos != *t_pos {
+						if let Some(t_tile)	= &self.tile_at_pos(t_pos) {
+							if t_tile.color != f_tile.color {
+								return MoveType::Eat;
+							}
+						} else {
+							return MoveType::Move;
+						}
+					}
+				},
 			}
 		}
-		return MoveType::Invalid;
+		return MoveType::Illegal;
 	}
 
-	fn generate_valid_moves(&self, f_pos: &Pos) -> Vec<Pos> {
+	fn move_type(&self, f_pos: &Pos, t_pos: &Pos) -> MoveType {
+		if let Some(f_tile) = &self.tile_at_pos(f_pos) {
+			if let Some(t_tile) = self.tile_at_pos(t_pos) {
+				if f_tile.color != t_tile.color {
+					return MoveType::Eat;
+				}
+			} else {
+				return MoveType::Move;
+			}
+		}
+		return MoveType::Illegal;
+	}
+	
+	fn generate_legal_moves(&self, f_pos: &Pos) -> Vec<Pos> {
 		let mut moves = Vec::new();
+
+		/* Generate possible moves from a given point in all directions */
+		fn generate_arm(moves:&mut Vec<Pos>, b:&Board, f_pos:&Pos, f_c:fn(i8,i8)->i8, f_r:fn(i8,i8)->i8) {
+			for i in 1..8 {
+				if let Some(t_pos) = Pos::at(f_c(f_pos.col,i), f_r(f_pos.row,i)) {
+					match b.move_type(f_pos,&t_pos) {
+						MoveType::Move => moves.push(t_pos),
+						MoveType::Eat => { moves.push(t_pos); break; },
+						MoveType::Illegal => break
+					}
+				} else { break; }
+			}
+		};
+
+		/* Generate possible bishop moves from a given point */
+		fn generate_bishop(moves:&mut Vec<Pos>, b:&Board, f_pos:&Pos) {
+			let add = ops::Add::add;
+			let sub = ops::Sub::sub;
+
+			/* north-west */
+			generate_arm(moves,b,f_pos,sub,add);
+			/* north-east */
+			generate_arm(moves,b,f_pos,add,add);
+			/* south-east */
+			generate_arm(moves,b,f_pos,add,sub);
+			/* south-west */
+			generate_arm(moves,b,f_pos,sub,sub);
+		}
+
+		/* Generate possible rook moves from a given point */
+		fn generate_rook(moves:&mut Vec<Pos>, b:&Board, f_pos:&Pos) {
+			let add = ops::Add::add;
+			let sub = ops::Sub::sub;
+			let id = |x,_| x;
+
+			/* north */
+			generate_arm(moves,b,f_pos,id,add);
+			/* east */
+			generate_arm(moves,b,f_pos,add,id);
+			/* south */
+			generate_arm(moves,b,f_pos,sub,id);
+			/* west */
+			generate_arm(moves,b,f_pos,id,sub);
+		}
+		
 		if let Some(f_tile) = &self.tile_at_pos(f_pos) {
 			match f_tile.piece {
 				Piece::Pawn => {
 					match f_tile.color {
 						Color::White => {
-							if f_pos.row == 1 {
-								if let Some(t_pos) = Pos::at(f_pos.col, f_pos.row+2) {
-									if self.move_type(f_pos,&t_pos) == MoveType::Move {
-										moves.push(t_pos);
+							if let Some(t_pos) = Pos::at(f_pos.col, f_pos.row+1) {
+								if self.move_type(f_pos,&t_pos) == MoveType::Move {
+									moves.push(t_pos);
+								}
+							} else {
+								if f_pos.row == 1 {
+									if let Some(t_pos) = Pos::at(f_pos.col, f_pos.row+2) {
+										if self.move_type(f_pos,&t_pos) == MoveType::Move {
+											moves.push(t_pos);
+										}
 									}
 								}
 							}
-							for i in [-1, 0, 1].iter() {
+							for i in [-1, 1].iter() {
 								if let Some(t_pos) = Pos::at(f_pos.col+i, f_pos.row+1) {
-									if self.move_type(f_pos,&t_pos) != MoveType::Invalid {
+									if self.move_type(f_pos,&t_pos) == MoveType::Eat {
 										moves.push(t_pos);
 									}
 								}
 							}
 						}
 						Color::Black => {
-							if f_pos.row == 6 {
-								if let Some(t_pos) = Pos::at(f_pos.col, f_pos.row-2) {
-									if self.move_type(f_pos,&t_pos) == MoveType::Move {
-										moves.push(t_pos);
+							if let Some(t_pos) = Pos::at(f_pos.col, f_pos.row-1) {
+								if self.move_type(f_pos,&t_pos) == MoveType::Move {
+									moves.push(t_pos);
+								}
+							} else {
+								if f_pos.row == 6 {
+									if let Some(t_pos) = Pos::at(f_pos.col, f_pos.row-2) {
+										if self.move_type(f_pos,&t_pos) == MoveType::Move {
+											moves.push(t_pos);
+										}
 									}
 								}
 							}
-							for i in [-1, 0, 1].iter() {
+							for i in [-1, 1].iter() {
 								if let Some(t_pos) = Pos::at(f_pos.col+i, f_pos.row-1) {
-									if self.move_type(f_pos,&t_pos) != MoveType::Invalid {
+									if self.move_type(f_pos,&t_pos) == MoveType::Eat {
 										moves.push(t_pos);
 									}
 								}
@@ -203,89 +353,20 @@ impl Board {
 					];
 					for (c,r) in tps.iter() {
 						if let Some(t_pos) = Pos::at(*c,*r) {
-							if self.move_type(f_pos,&t_pos) != MoveType::Invalid {
+							if self.move_type(f_pos,&t_pos) != MoveType::Illegal {
 								moves.push(t_pos);
 							}
 						}
 					}
 				},
 
-				Piece::Bishop | Piece::Queen => {
-					/* north-west */
-					for i in 1..8 {
-						if let Some(t_pos) = Pos::at(f_pos.col-i, f_pos.row+i) {
-							match self.move_type(f_pos,&t_pos) {
-								MoveType::Move | MoveType::Eat => moves.push(t_pos),
-								MoveType::Eat | MoveType::Invalid => break
-							}
-						} else { break; }
-					}
-					/* north-east */
-					for i in 1..8 {
-						if let Some(t_pos) = Pos::at(f_pos.col+i, f_pos.row+i) {
-							match self.move_type(f_pos,&t_pos) {
-								MoveType::Move | MoveType::Eat => moves.push(t_pos),
-								MoveType::Eat | MoveType::Invalid => break
-							}
-						} else { break; }
-					}
-					/* south-east */
-					for i in 1..8 {
-						if let Some(t_pos) = Pos::at(f_pos.col+i, f_pos.row-i) {
-							match self.move_type(f_pos,&t_pos) {
-								MoveType::Move | MoveType::Eat => moves.push(t_pos),
-								MoveType::Eat | MoveType::Invalid => break
-							}
-						} else { break; }
-					}
-					/* south-west */
-					for i in 1..8 {
-						if let Some(t_pos) = Pos::at(f_pos.col-i, f_pos.row-i) {
-							match self.move_type(f_pos,&t_pos) {
-								MoveType::Move | MoveType::Eat => moves.push(t_pos),
-								MoveType::Eat | MoveType::Invalid => break
-							}
-						} else { break; }
-					}
-				},
+				Piece::Bishop => generate_bishop(&mut moves,self,f_pos),
 
-				Piece::Rook | Piece::Queen => {
-					/* north */
-					for i in 1..8 {
-						if let Some(t_pos) = Pos::at(f_pos.col, f_pos.row+i) {
-							match self.move_type(f_pos,&t_pos) {
-								MoveType::Move | MoveType::Eat => moves.push(t_pos),
-								MoveType::Eat | MoveType::Invalid => break
-							}
-						} else { break; }
-					}
-					/* east */
-					for i in 1..8 {
-						if let Some(t_pos) = Pos::at(f_pos.col+i, f_pos.row) {
-							match self.move_type(f_pos,&t_pos) {
-								MoveType::Move | MoveType::Eat => moves.push(t_pos),
-								MoveType::Eat | MoveType::Invalid => break
-							}
-						} else { break; }
-					}
-					/* south */
-					for i in 1..8 {
-						if let Some(t_pos) = Pos::at(f_pos.col, f_pos.row-i) {
-							match self.move_type(f_pos,&t_pos) {
-								MoveType::Move | MoveType::Eat => moves.push(t_pos),
-								MoveType::Eat | MoveType::Invalid => break
-							}
-						} else { break; }
-					}
-					/* west */
-					for i in 1..8 {
-						if let Some(t_pos) = Pos::at(f_pos.col-i, f_pos.row) {
-							match self.move_type(f_pos,&t_pos) {
-								MoveType::Move | MoveType::Eat => moves.push(t_pos),
-								MoveType::Eat | MoveType::Invalid => break
-							}
-						} else { break; }
-					}
+				Piece::Rook  => generate_rook(&mut moves,self,f_pos),
+
+				Piece::Queen => {
+					generate_bishop(&mut moves,self,f_pos);
+					generate_rook(&mut moves,self,f_pos);
 				},
 
 				Piece::King => {
@@ -293,15 +374,14 @@ impl Board {
 						for r in [-1, 0, 1].iter() {
 							if *c != 0 || *r != 0 {
 								if let Some(t_pos) = Pos::at(f_pos.col+*c, f_pos.row+*r) {
-									if self.move_type(f_pos,&t_pos) != MoveType::Invalid {
+									if self.move_type(f_pos,&t_pos) != MoveType::Illegal {
 										moves.push(t_pos);
 									}
 								}
 							}
 						}
 					}
-		},
-				_ => ()
+				}
 			}
 		}
 		return moves;
@@ -320,48 +400,6 @@ impl Valuable for Board {
 	}
 }
 
-#[derive(Debug)]
-struct Pos {
-	// we use i8 instead of u8
-	// to avoid overflows when generating positions that are possibly outside the board
-	row: i8,
-	col: i8,
-}
-
-impl Pos {
-	fn at(col: i8, row:i8) -> Option<Pos> {
-		if col < 0 || row < 0 || col > 7 || row > 7 {
-			return None;
-		}
-		return Some(Pos {
-			col: col,
-			row: row,
-	});
-	}
-
-	fn from_coord(x: char, y: char) -> Option<Pos> {
-		if ! (x.is_ascii() && y.is_ascii()) {
-			return None;
-		}
-		let x = x.to_ascii_uppercase();
-
-		if ! (x.ge(&'A') && x.le(&'H') && y.ge(&'1') && y.le(&'8')) {
-			return None;
-		}
-
-		return Some(Pos {
-			col: ((x as u8) - b'A') as i8,
-			row: ((y as u8) - b'1') as i8,
-		});
-	}
-
-	fn to_coord(&self) -> (char,char) {
-		return (
-			((self.col as u8) + b'A') as char,
-			((self.row as u8) + b'1') as char
-		);
-	}
-}
 
 fn main() {
 
@@ -385,7 +423,7 @@ fn main() {
 			if let Some(f_tile) = b.tile_at_pos(&f_pos) {
 				if f_tile.color == Color::White {
 					print!("[{},{}] ({:?}): ",c,r,f_tile.piece);
-					println!("{:?}", b.generate_valid_moves(&f_pos));
+					println!("{:?}", b.generate_legal_moves(&f_pos));
 				}
 			}
 		}
