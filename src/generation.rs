f35ae49fd36f24ace8570491a871f4f67cee182d
Roberto Cornacchia <roberto@spinque.com>
2021-02-28 23:49:44 +0100

    partial implementation of testing for chek

--- src/generation.rs
+++ src/generation.rs
@@ -3,6 +3,48 @@ use std::ops;
 use crate::board::{Board,Pos,Move,MoveType,Piece,Color};
 
 impl Board {
+
+	/// Check if the King of the given color is in check
+	pub fn is_check_for_color(&self, color: Color) -> bool {
+		let king_pos = self.king_pos[color as usize];
+		let f_incr:fn(i8,i8)->i8;
+
+		/* check pawn attack */
+		if color == Color::White {
+			f_incr = ops::Add::add;
+		} else {
+			f_incr = ops::Sub::sub;
+		}
+		for i in [-1, 1].iter() {
+			if let Some(pos) = Pos::at(king_pos.col+i, f_incr(king_pos.row,1)) {
+				if let MoveType::Capture(_) = self.move_type(king_pos,pos) {
+					return true;
+				}
+			}
+		}
+
+		/* check all other attacks, by reversing an attack from the king */
+		let mut moves = Vec::new();
+		self.generate_bishop(&mut moves, king_pos, 7);
+		if moves.iter().any(|&p| { self.at(p).is_some() }) {
+			return true
+		}
+
+		let mut moves = Vec::new();
+		self.generate_knight(&mut moves, king_pos);
+		if moves.iter().any(|&p| { self.at(p).is_some() }) {
+			return true
+		}
+	
+		let mut moves = Vec::new();
+		self.generate_rook(&mut moves, king_pos, 7);
+		if moves.iter().any(|&p| { self.at(p).is_some() }) {
+			return true
+		}
+
+		false
+	}
+
 	fn move_type(&self, f_pos: Pos, t_pos: Pos) -> MoveType {
 		if let Some(f_tile) = self.at(f_pos) {
 			if let Some(t_tile) = self.at(t_pos) {
@@ -118,7 +160,7 @@ impl Board {
 					}
 					if let Some(t_tile) = self.at(t_pos) {
 						/* Capture diagonally */
-						if (t_pos.col -f_pos.col).abs() == 1 &&
+						if (t_pos.col - f_pos.col).abs() == 1 &&
 								t_pos.row == f_incr(f_pos.row,1) &&
 								t_tile.color != f_tile.color {
 							return MoveType::Capture(t_tile.piece);
@@ -204,7 +246,132 @@ impl Board {
 		MoveType::Illegal
 	}
 
-	
+	/// Generate possible Pawn moves from a given point
+	fn generate_pawn(&self, moves: &mut Vec<Pos>, f_pos: Pos) {
+		let f_incr:fn(i8,i8)->i8;
+		let start_row:i8;
+
+		if let Some(f_tile) = self.at(f_pos) {
+			if f_tile.color == Color::White {
+				f_incr = ops::Add::add;
+				start_row = 1;
+			} else {
+				f_incr = ops::Sub::sub;
+				start_row = 6;
+			}
+
+			/* forward by 1 */
+			if let Some(t_pos) = Pos::at(f_pos.col, f_incr(f_pos.row,1)) {
+				if self.move_type(f_pos,t_pos) == MoveType::Move {
+					moves.push(t_pos);
+					if f_pos.row == start_row {
+						/* forward by 2 */
+						if let Some(t_pos) = Pos::at(f_pos.col, f_incr(f_pos.row,2)) {
+							if self.move_type(f_pos,t_pos) == MoveType::Move {
+								moves.push(t_pos);
+							}
+						}
+					}
+				}
+			}
+			/* Capture diagonally */
+			for i in [-1, 1].iter() {
+				if let Some(t_pos) = Pos::at(f_pos.col+i, f_incr(f_pos.row,1)) {
+					if let MoveType::Capture(_) = self.move_type(f_pos,t_pos) {
+						moves.push(t_pos);
+					}
+				}
+			}
+		}
+	}
+
+	/// Generate possible Knight moves from a given point
+	fn generate_knight(&self, moves: &mut Vec<Pos>, f_pos: Pos) {
+		let tps = [
+			(f_pos.col-1,f_pos.row-2),(f_pos.col-1,f_pos.row+2),
+			(f_pos.col+1,f_pos.row-2),(f_pos.col+1,f_pos.row+2),
+			(f_pos.col-2,f_pos.row-1),(f_pos.col-2,f_pos.row+1),
+			(f_pos.col+2,f_pos.row-1),(f_pos.col+2,f_pos.row+1),
+		];
+		for (c,r) in tps.iter() {
+			if let Some(t_pos) = Pos::at(*c,*r) {
+				if self.move_type(f_pos,t_pos) != MoveType::Illegal {
+					moves.push(t_pos);
+				}
+			}
+		}
+	}
+
+		/// Generate possible straight and diagonal moves from a given point in any directions
+	fn generate_arm(&self, moves: &mut Vec<Pos>, f_pos: Pos, max_len: i8, f_c:fn(i8,i8)->i8, f_r:fn(i8,i8)->i8) {
+		for i in 1..max_len+1 {
+			if let Some(t_pos) = Pos::at(f_c(f_pos.col,i), f_r(f_pos.row,i)) {
+				match self.move_type(f_pos,t_pos) {
+					MoveType::Move => moves.push(t_pos),
+					MoveType::Capture(_) => { moves.push(t_pos); break; },
+					MoveType::Illegal => break
+				}
+			} else { break; }
+		}
+	}
+
+	/// Generate possible Bishop moves from a given point
+	fn generate_bishop(&self, moves: &mut Vec<Pos>, f_pos: Pos, max_len: i8) {
+		let add = ops::Add::add;
+		let sub = ops::Sub::sub;
+
+		/* north-west */
+		self.generate_arm(moves,f_pos,max_len,sub,add);
+		/* north-east */
+		self.generate_arm(moves,f_pos,max_len,add,add);
+		/* south-east */
+		self.generate_arm(moves,f_pos,max_len,add,sub);
+		/* south-west */
+		self.generate_arm(moves,f_pos,max_len,sub,sub);
+	}
+
+	/// Generate possible Rook moves from a given point
+	fn generate_rook(&self, moves: &mut Vec<Pos>, f_pos: Pos, max_len: i8) {
+		let add = ops::Add::add;
+		let sub = ops::Sub::sub;
+		let id = |x,_| x;
+
+		/* north */
+		self.generate_arm(moves,f_pos,max_len,id,add);
+		/* east */
+		self.generate_arm(moves,f_pos,max_len,add,id);
+		/* south */
+		self.generate_arm(moves,f_pos,max_len,sub,id);
+		/* west */
+		self.generate_arm(moves,f_pos,max_len,id,sub);
+	}
+
+	/// Generate possible Queen moves from a given point
+	fn generate_queen(&self, moves: &mut Vec<Pos>, f_pos: Pos, max_len: i8) {
+		self.generate_bishop(moves,f_pos,max_len);
+		self.generate_rook(moves,f_pos,max_len);
+	}
+
+	/// Generate possible King moves from a given point
+	fn generate_king(&self, moves: &mut Vec<Pos>, f_pos: Pos) {
+		self.generate_bishop(moves,f_pos,1);
+		self.generate_rook(moves,f_pos,1);
+
+		/* castling (TODO: cannot be used in currently in check) */
+		if self.can_castle_left[self.player as usize] {
+			if self.at(Pos::at(f_pos.col-1,f_pos.row).unwrap()).is_none() &&
+				self.at(Pos::at(f_pos.col-2,f_pos.row).unwrap()).is_none() {
+					moves.push(Pos::at(f_pos.col-2,f_pos.row).unwrap());
+			}
+		}
+		if self.can_castle_right[self.player as usize] {
+			if self.at(Pos::at(f_pos.col+1,f_pos.row).unwrap()).is_none() &&
+				self.at(Pos::at(f_pos.col+2,f_pos.row).unwrap()).is_none() {
+					moves.push(Pos::at(f_pos.col+2,f_pos.row).unwrap());
+			}
+		}
+	}
+
 	pub fn generate_all_legal_moves(&self) -> Vec<Move> {
 		let mut all_moves = Vec::new();
 		for c in 0..8 {
@@ -216,8 +383,8 @@ impl Board {
 						for t_pos in moves {
 							let promotion;
 							if f_tile.piece == Piece::Pawn 
-							 && ((f_tile.color == Color::Black && f_pos.row == 0)
-							  || (f_tile.color == Color::White && f_pos.row == 7)) {
+								&& ((f_tile.color == Color::Black && f_pos.row == 0)
+								|| (f_tile.color == Color::White && f_pos.row == 7)) {
 										/* simplification: assume we always promote to a Queen */
 								promotion = Some(Piece::Queen);
 							} else {
@@ -234,131 +401,15 @@ impl Board {
 
 	pub fn generate_legal_moves(&self, f_pos: Pos) -> Vec<Pos> {
 		let mut moves = Vec::new();
-
-		/// Generate possible moves from a given point in all directions
-		fn generate_arm(moves: &mut Vec<Pos>, b: &Board, f_pos: Pos, max_len: i8, f_c:fn(i8,i8)->i8, f_r:fn(i8,i8)->i8) {
-			for i in 1..max_len+1 {
-				if let Some(t_pos) = Pos::at(f_c(f_pos.col,i), f_r(f_pos.row,i)) {
-					match b.move_type(f_pos,t_pos) {
-						MoveType::Move => moves.push(t_pos),
-						MoveType::Capture(_) => { moves.push(t_pos); break; },
-						MoveType::Illegal => break
-					}
-				} else { break; }
-			}
-		};
-
-		/// Generate possible bishop moves from a given point
-		fn generate_bishop(moves: &mut Vec<Pos>, b: &Board, f_pos: Pos, max_len: i8) {
-			let add = ops::Add::add;
-			let sub = ops::Sub::sub;
-
-			/* north-west */
-			generate_arm(moves,b,f_pos,max_len,sub,add);
-			/* north-east */
-			generate_arm(moves,b,f_pos,max_len,add,add);
-			/* south-east */
-			generate_arm(moves,b,f_pos,max_len,add,sub);
-			/* south-west */
-			generate_arm(moves,b,f_pos,max_len,sub,sub);
-		}
-
-		/// Generate possible rook moves from a given point
-		fn generate_rook(moves: &mut Vec<Pos>, b: &Board, f_pos: Pos, max_len: i8) {
-			let add = ops::Add::add;
-			let sub = ops::Sub::sub;
-			let id = |x,_| x;
-
-			/* north */
-			generate_arm(moves,b,f_pos,max_len,id,add);
-			/* east */
-			generate_arm(moves,b,f_pos,max_len,add,id);
-			/* south */
-			generate_arm(moves,b,f_pos,max_len,sub,id);
-			/* west */
-			generate_arm(moves,b,f_pos,max_len,id,sub);
-		}
 		
 		if let Some(f_tile) = self.at(f_pos) {
 			match f_tile.piece {
-				Piece::Pawn => {
-					let f_incr:fn(i8,i8)->i8;
-					let start_row:i8;
-
-					if f_tile.color == Color::White {
-						f_incr = ops::Add::add;
-						start_row = 1;
-					} else {
-						f_incr = ops::Sub::sub;
-						start_row = 6;
-					}
-
-					/* forward by 1 */
-					if let Some(t_pos) = Pos::at(f_pos.col, f_incr(f_pos.row,1)) {
-						if self.move_type(f_pos,t_pos) == MoveType::Move {
-							moves.push(t_pos);
-							if f_pos.row == start_row {
-								/* forward by 2 */
-								if let Some(t_pos) = Pos::at(f_pos.col, f_incr(f_pos.row,2)) {
-									if self.move_type(f_pos,t_pos) == MoveType::Move {
-										moves.push(t_pos);
-									}
-								}
-							}
-						}
-					}
-					/* Capture diagonally */
-					for i in [-1, 1].iter() {
-						if let Some(t_pos) = Pos::at(f_pos.col+i, f_incr(f_pos.row,1)) {
-							if let MoveType::Capture(_) = self.move_type(f_pos,t_pos) {
-								moves.push(t_pos);
-							}
-						}
-					}
-				},
-
-				Piece::Knight => {
-					let tps = [
-						(f_pos.col-1,f_pos.row-2),(f_pos.col-1,f_pos.row+2),
-						(f_pos.col+1,f_pos.row-2),(f_pos.col+1,f_pos.row+2),
-						(f_pos.col-2,f_pos.row-1),(f_pos.col-2,f_pos.row+1),
-						(f_pos.col+2,f_pos.row-1),(f_pos.col+2,f_pos.row+1),
-					];
-					for (c,r) in tps.iter() {
-						if let Some(t_pos) = Pos::at(*c,*r) {
-							if self.move_type(f_pos,t_pos) != MoveType::Illegal {
-								moves.push(t_pos);
-							}
-						}
-					}
-				},
-
-				Piece::Bishop => generate_bishop(&mut moves,self,f_pos,7),
-
-				Piece::Rook  => generate_rook(&mut moves,self,f_pos,7),
-
-				Piece::Queen => {
-					generate_bishop(&mut moves,self,f_pos,7);
-					generate_rook(&mut moves,self,f_pos,7);
-				},
-
-				Piece::King => {
-					generate_bishop(&mut moves,self,f_pos,1);
-					generate_rook(&mut moves,self,f_pos,1);
-					/* castling (TODO: cannot be used in currently in check) */
-					if self.can_castle_left[self.player as usize] {
-						if self.at(Pos::at(f_pos.col-1,f_pos.row).unwrap()).is_none() &&
-						   self.at(Pos::at(f_pos.col-2,f_pos.row).unwrap()).is_none() {
-								moves.push(Pos::at(f_pos.col-2,f_pos.row).unwrap());
-						}
-					}
-					if self.can_castle_right[self.player as usize] {
-						if self.at(Pos::at(f_pos.col+1,f_pos.row).unwrap()).is_none() &&
-						   self.at(Pos::at(f_pos.col+2,f_pos.row).unwrap()).is_none() {
-								moves.push(Pos::at(f_pos.col+2,f_pos.row).unwrap());
-						}
-					}
-				}
+				Piece::Pawn => self.generate_pawn(&mut moves,f_pos),
+				Piece::Knight => self.generate_knight(&mut moves,f_pos),
+				Piece::Bishop => self.generate_bishop(&mut moves,f_pos,7),
+				Piece::Rook  => self.generate_rook(&mut moves,f_pos,7),
+				Piece::Queen => self.generate_queen(&mut moves,f_pos,7),
+				Piece::King => self.generate_king(&mut moves,f_pos),
 			}
 		}
 		moves
