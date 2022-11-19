b824e9caaefa1644d8d0eb1e66b2a081dd4d86d9
Roberto Cornacchia <roberto@spinque.com>
2022-11-19 19:27:57 +0100

    Allow to restart a game and choose color

--- duchess.js
+++ duchess.js
@@ -7,6 +7,26 @@ var $last_duchess = $('#last_duchess')
 var last_human_fen = null
 var last_duchess_fen = null
 
+$('#new_game_as_white').on('click', function () {
+  board.orientation('white')
+  board.position('start')
+  $last_duchess.html(null)
+  $last_human.html(null)
+  last_human_fen = board.fen() + ' w KQkq - 0 1'
+  last_duchess_fen = board.fen() + ' w KQkq - 0 1'
+})
+
+$('#new_game_as_black').on('click', function () {
+  board.orientation('black')
+  board.position('start')
+  $last_duchess.html(null)
+  $last_human.html(null)
+  last_human_fen = board.fen() + ' w KQkq - 0 1'
+  last_duchess_fen = board.fen() + ' w KQkq - 0 1'
+
+  window.setTimeout(duchessMove, 1000, last_human_fen)
+})
+
 function duchessMove(fromFEN) {
   last_duchess_fen = best_move(fromFEN)
   board.position(last_duchess_fen)
@@ -14,7 +34,7 @@ function duchessMove(fromFEN) {
 }
 
 function onDrop (source, target, piece, newPos, oldPos, orientation) {
-  // make move returns a new FEN, or 'illegal'
+  // make  move returns a new FEN, or 'illegal'
   var fen = make_move(last_duchess_fen, source, target)
   if(fen == 'illegal') {
     return 'snapback'
@@ -34,10 +54,7 @@ export function duchess () {
     draggable: true,
     position: 'start',
     orientation: 'white',
-    // onDragStart: onDragStart,
     onDrop: onDrop,
-    // onSnapEnd: onSnapEnd
-    // onMoveEnd: onMoveEnd
   }
   board = Chessboard('board1', config)
   last_human_fen = board.fen() + ' w KQkq - 0 1'
