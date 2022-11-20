dbfd473f2c14e84eb56387b8d55b5cfed59d6662
Roberto Cornacchia <roberto@spinque.com>
2022-11-20 22:49:57 +0100

    Make the ui check for end game

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 extern crate wasm_bindgen;
 use std::str::FromStr;
 
+use clap::ColorChoice;
 use wasm_bindgen::prelude::*;
 
 mod board;
@@ -13,30 +14,31 @@ mod misc;
 mod negascout;
 mod ordering;
 // mod uci;
-use crate::board::{Board,Pos, Move, MoveType};
+use crate::board::{Board,Pos, Move, MoveType, GameEnd, Color};
 use crate::negascout::negascout;
 use crate::misc::OPTS_DEFAULT;
 
 #[wasm_bindgen]
-pub fn best_move(fromFEN:&str) -> String {
+/// Computes the best move from the given board
+pub fn find_best_move(fromFEN:&str) -> String {
   let b = Board::from_fen(fromFEN).unwrap();
   let (_, mv) = negascout(&b, &OPTS_DEFAULT);
   return b.clone_apply_move(&mv).to_fen();
 }
 
 #[wasm_bindgen]
-/// checks that the move is legal e returns a new FEN for the opponent
+/// Checks that the move is legal e returns a new FEN for the opponent
 pub fn make_move(fromFEN:&str, fromPos:&str, toPos:&str) -> String {
   let b = match Board::from_fen(fromFEN) {
-    Err(_) => return "illegal".to_string(),
+    Err(_) => return "illegal_input".to_string(),
     Ok(v) => v
   };
   let f_pos = match Pos::from_str(fromPos) {
-    Err(_) => return "illegal".to_string(),
+    Err(_) => return "illegal_input".to_string(),
     Ok(v) => v
   };
   let t_pos = match Pos::from_str(toPos) {
-    Err(_) => return "illegal".to_string(),
+    Err(_) => return "illegal_input".to_string(),
     Ok(v) => v
   };
   let mt = b.check_move(f_pos, t_pos, 0);
@@ -46,4 +48,19 @@ pub fn make_move(fromFEN:&str, fromPos:&str, toPos:&str) -> String {
       return b.clone_apply_move(&Move{f_pos, t_pos}).to_fen();
     }
   }
+}
+
+#[wasm_bindgen]
+/// Reports whether and in what way the game ended
+pub fn check_end_game(fromFEN:&str) -> String {
+  let b = match Board::from_fen(fromFEN) {
+    Err(_) => return "illegal_input".to_string(),
+    Ok(v) => v
+  };
+  match b.check_end_game() {
+    Some(GameEnd::Draw) => return "draw".to_string(),
+    Some(GameEnd::Checkmate(Color::Black)) => return "checkmate black".to_string(),
+    Some(GameEnd::Checkmate(Color::White)) => return "checkmate white".to_string(),
+    None => return "none".to_string()
+  };
 }
